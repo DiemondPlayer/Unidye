9e9d59daecd196320b68dfe3c3f55850f3f4dea3
Diemond_Player <diemondplay@gmail.com>
2025-02-01 19:20:19 +0500

    dog and cat fix

--- src/main/java/net/diemond_player/unidye/mixin/CatCollarFeatureRendererMixin.java
+++ src/main/java/net/diemond_player/unidye/mixin/CatCollarFeatureRendererMixin.java
@@ -1,8 +1,10 @@
 package net.diemond_player.unidye.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.diemond_player.unidye.util.UnidyeAccessor;
 import net.diemond_player.unidye.util.UnidyeUtils;
 import net.minecraft.client.render.entity.feature.CatCollarFeatureRenderer;
+import net.minecraft.entity.passive.CatEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -12,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public abstract class CatCollarFeatureRendererMixin {
 
     @ModifyArgs(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/CatEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/feature/CatCollarFeatureRenderer;render(Lnet/minecraft/client/render/entity/model/EntityModel;Lnet/minecraft/client/render/entity/model/EntityModel;Lnet/minecraft/util/Identifier;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFFFFF)V"))
-    private void unidye$render(Args args) {
-        UnidyeAccessor cat = (UnidyeAccessor) args.get(6);
+    private void unidye$render(Args args, @Local(argsOnly = true) CatEntity catEntity) {
+        UnidyeAccessor cat = (UnidyeAccessor) catEntity;
         int customColor = cat.unidye$getCustomColor();
         if (customColor != 0xFFFFFF) {
             float[] fs = UnidyeUtils.getColorArray(customColor);
--- src/main/java/net/diemond_player/unidye/mixin/WolfCollarFeatureRendererMixin.java
+++ src/main/java/net/diemond_player/unidye/mixin/WolfCollarFeatureRendererMixin.java
@@ -1,8 +1,10 @@
 package net.diemond_player.unidye.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.diemond_player.unidye.util.UnidyeAccessor;
 import net.diemond_player.unidye.util.UnidyeUtils;
 import net.minecraft.client.render.entity.feature.WolfCollarFeatureRenderer;
+import net.minecraft.entity.passive.WolfEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -12,14 +14,14 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public abstract class WolfCollarFeatureRendererMixin {
 
     @ModifyArgs(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/WolfEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/WolfEntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
-    private void unidye$render(Args args) {
-        UnidyeAccessor wolf = (UnidyeAccessor) args.get(5);
+    private void unidye$render(Args args, @Local(argsOnly = true) WolfEntity wolfEntity) {
+        UnidyeAccessor wolf = (UnidyeAccessor) wolfEntity;
         int customColor = wolf.unidye$getCustomColor();
         if (customColor != 0xFFFFFF) {
             float[] fs = UnidyeUtils.getColorArray(customColor);
-            args.set(6, fs[0]);
-            args.set(7, fs[1]);
-            args.set(8, fs[2]);
+            args.set(4, fs[0]);
+            args.set(5, fs[1]);
+            args.set(6, fs[2]);
         }
     }
 }
