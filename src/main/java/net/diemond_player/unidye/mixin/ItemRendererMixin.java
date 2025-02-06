055caa4db3fed535bd1d3bf7307181826a0bb0ae
Diemond_Player <diemondplay@gmail.com>
2025-02-06 18:05:50 +0500

    1.2.2 ver

--- src/main/java/net/diemond_player/unidye/mixin/ItemRendererMixin.java
+++ src/main/java/net/diemond_player/unidye/mixin/ItemRendererMixin.java
@@ -1,9 +1,9 @@
 package net.diemond_player.unidye.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.diemond_player.unidye.block.UnidyeBlocks;
-import net.diemond_player.unidye.item.UnidyeItems;
+import net.diemond_player.unidye.item.UnidyeItemGroups;
 import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +13,11 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class ItemRendererMixin {
     @ModifyArg(method = "renderBakedItemQuads", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumer;quad(Lnet/minecraft/client/util/math/MatrixStack$Entry;Lnet/minecraft/client/render/model/BakedQuad;FFFFII)V"), index = 5)
     private float unidye$renderBakedItemQuads(float alpha, @Local(argsOnly = true) ItemStack stack) {
-        if(stack.isOf(UnidyeItems.CUSTOM_DYE) || stack.isOf(UnidyeBlocks.CUSTOM_STAINED_GLASS.asItem()) || stack.isOf(UnidyeBlocks.CUSTOM_STAINED_GLASS_PANE.asItem())) {
+        if(stack.getItem() instanceof BlockItem){
+            if(UnidyeItemGroups.UNIDYE_ITEM_GROUP_ITEMS.contains(((BlockItem)stack.getItem()).getBlock())) {
+                return 1.0f;
+            }
+        } else if(UnidyeItemGroups.UNIDYE_ITEM_GROUP_ITEMS.contains(stack.getItem())) {
             return 1.0f;
         }
         return alpha;
